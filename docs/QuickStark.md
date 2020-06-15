3b7202d1ba33e589519e2858a47226441b4673c7
Yuan Rui <njuyuanrui@163.com>
2020-06-15 11:36:15 +0800

    prerequisites

--- docs/QuickStark.md
+++ docs/QuickStark.md
@@ -1,1 +1,11 @@
-TBD
+
+## Prerequisites
+
+* Ubuntu 16.04
+* Python3 & pip3
+* Chrome Browser and [Chrome Driver](http://chromedriver.storage.googleapis.com/index.html) in the same version
+
+## Installation
+```
+  $ ./install_deps.sh
+```
--- install_deps.sh
+++ install_deps.sh
@@ -1,4 +1,2 @@
-sudo apt-get install build-essential
-sudo apt-get install libgtk-3-dev libgstreamer-plugins-base1.0-dev
-sudo apt-get install libwebkitgtk-3.0-dev
+sudo apt-get install build-essential libgtk-3-dev libgstreamer-plugins-base1.0-dev  libwebkitgtk-3.0-dev libxslt-dev freeglut3-dev  
 pip3 install -r requirements.txt
