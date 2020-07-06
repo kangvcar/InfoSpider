1732b58e986afd516f099353d05a99c773452d4a
kangvcar <kangvcar@126.com>
2020-07-06 19:55:46 +0800

    更新github脚本

---
+++ tests/github/main.py
@@ -0,0 +1,98 @@
+import json
+import os
+import re
+import requests
+
+class Github(object):
+    def __init__(self, username):
+        self.username = username
+        self.headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36',
+        }
+
+        self.proxypool_url = 'http://127.0.0.1:5555/random'
+        self.target_url = 'http://httpbin.org/get'
+
+    def get_random_proxy(self):
+        """
+        get random proxy from proxypool
+        :return: proxy
+        """
+        proxies = {'http': 'https://' + requests.get(self.proxypool_url).text.strip()}
+        print(proxies)
+        return proxies
+
+    # 用户信息
+    def get_user_info(self):
+        url = 'https://api.github.com/users/' + self.username
+        resp = requests.get(url, headers=self.headers, proxies=self.get_random_proxy())
+        # print(resp.text)
+        file_path = os.path.join(os.path.dirname(__file__) + '/user_infomation.json')
+        with open(file_path, 'w') as f:
+            f.write(resp.text.encode("gbk", 'ignore').decode("gbk", "ignore"))
+        return file_path
+
+    # 用户仓库信息
+    def get_user_repos(self):
+        url = 'https://api.github.com/users/' + self.username + '/repos'
+        resp = requests.get(url, headers=self.headers, proxies=self.get_random_proxy())
+        # print(resp.text)
+        file_path = os.path.join(os.path.dirname(__file__) + '/user_repository.json')
+        with open(file_path, 'w') as f:
+            f.write(resp.text.encode("gbk", 'ignore').decode("gbk", "ignore"))
+        return file_path
+
+    # 用户的关注信息
+    def get_user_following(self):
+        url = 'https://api.github.com/users/' + self.username + '/following'
+        resp = requests.get(url, headers=self.headers, proxies=self.get_random_proxy())
+        print(resp.text)
+        file_path = os.path.join(os.path.dirname(__file__) + '/user_following.json')
+        with open(file_path, 'w') as f:
+            f.write(resp.text.encode("gbk", 'ignore').decode("gbk", "ignore"))
+        return file_path
+
+    # 用户的粉丝信息
+    def get_user_followers(self):
+        url = 'https://api.github.com/users/' + self.username + '/followers'
+        resp = requests.get(url, headers=self.headers, proxies=self.get_random_proxy())
+        # print(resp.text)
+        file_path = os.path.join(os.path.dirname(__file__) + '/user_followers.json')
+        with open(file_path, 'w') as f:
+            f.write(resp.text.encode("gbk", 'ignore').decode("gbk", "ignore"))
+        return file_path
+
+    # 用户activity信息
+    def get_user_activity(self):
+        url = 'https://api.github.com/users/' + self.username + '/received_events'
+        resp = requests.get(url, headers=self.headers, proxies=self.get_random_proxy())
+        print(resp.text)
+        file_path = os.path.join(os.path.dirname(__file__) + '/user_activity.json')
+        with open(file_path, 'w') as f:
+            f.write(resp.text.encode("gbk", 'ignore').decode("gbk", "ignore"))
+        return file_path
+
+    # 用户所有仓库的详细信息
+    def get_user_repos_detail(self):
+        url = 'https://api.github.com/users/' + self.username + '/repos'
+        resp = requests.get(url, headers=self.headers, verify=False, timeout=2)
+        repo_detail = []
+        for name in resp.json():
+            repo_url = 'https://api.github.com/repos/' + self.username + '/' + name['name']
+            detail = requests.get(repo_url, headers=self.headers, verify=False, timeout=2)
+            repo_detail.append(detail.text.encode("gbk", 'ignore').decode("gbk", "ignore"))
+            print('正在下载仓库信息 >>> ', name['name'])
+        print(repo_detail)
+        file_path = os.path.join(os.path.dirname(__file__) + '/user_all_repos_detail.json')
+        with open(file_path, 'w') as f:
+            f.write(str(repo_detail))
+        return file_path
+
+if __name__ == '__main__':
+    github = Github('kangvcar')
+    github.get_user_info()
+    github.get_user_repos()
+    github.get_user_following()
+    github.get_user_followers()
+    github.get_user_activity()
+    # github.get_user_repos_detail()
