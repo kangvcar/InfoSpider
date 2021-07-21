a4e3974ba9f8fe5801cf4fd2f6cee42c9cc70919
kangvcar <kangvcar@126.com>
2021-07-21 10:25:41 +0800

    delete version 1.0

--- install_deps.sh
+++
@@ -1,2 +0,0 @@
-sudo apt-get install build-essential libgtk-3-dev libgstreamer-plugins-base1.0-dev  libwebkitgtk-3.0-dev libxslt-dev freeglut3-dev  
-pip3 install -r requirements.txt
--- uitest/main.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-import wx
-
